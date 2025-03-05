76b748f28ef6ea4a742f24ac20987422df53ee4b
marko <markovmaks895@gmail.com>
2025-03-05 14:28:16 +0300

    Создан дата слой для сетевых операций

---
+++ data/src/main/java/com/example/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.example.data.repository
+
+import com.example.data.network.NewsRemoteDataSource
+import com.example.domain.model.News
+import com.example.domain.model.NewsDetails
+import com.example.domain.repository.NewsRepository
+
+class NewsRepositoryImpl(
+    private val remoteDataSource: NewsRemoteDataSource,
+    //private val localDataSource: NewsLocalDataSource
+) : NewsRepository {
+
+    override suspend fun getNews(offset: Int?, limit: Int?): List<News> {
+        val remoteNews = remoteDataSource.getNews(offset, limit)
+        return remoteNews
+    }
+
+    override suspend fun getNewsDetails(id: String): NewsDetails {
+        val remoteDataSource = remoteDataSource.getNewsDetails(id)
+        return remoteDataSource
+    }
+
+}
