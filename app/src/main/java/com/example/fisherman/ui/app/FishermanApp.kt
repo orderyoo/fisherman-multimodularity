5152d1ac36423068e34b10b437549d5d53bd6d82
order_yo <markovmaks895@gmail.com>
2025-04-21 03:52:48 +0300

    Небольшой рефакторинг

--- app/src/main/java/com/example/fisherman/ui/app/FishermanApp.kt
+++ app/src/main/java/com/example/fisherman/ui/app/FishermanApp.kt
@@ -1,13 +1,8 @@
 package com.example.fisherman.ui.app
 
-
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.fisherman.navigation.FishermanNavHost
@@ -38,12 +33,6 @@ fun FishermanApp() {
             )
         }
     ) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            FishermanNavHost(navController, startDestination)
-        }
+        FishermanNavHost(innerPadding, navController, startDestination)
     }
 }
